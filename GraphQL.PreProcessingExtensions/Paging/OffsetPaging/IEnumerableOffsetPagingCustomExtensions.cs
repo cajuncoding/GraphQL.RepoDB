5958aac114b270835ce055dc0c0b9289960d1a4e
Brandon Bernard <brandon.bernard@exlrt.com>
2021-10-16 01:59:45 -0500

    Initial changes for successful build but Unit Test failing due to Paging/Sorting handlers not being correctly initialized in v12.

--- GraphQL.PreProcessingExtensions/Paging/OffsetPaging/IEnumerableOffsetPagingCustomExtensions.cs
+++ GraphQL.PreProcessingExtensions/Paging/OffsetPaging/IEnumerableOffsetPagingCustomExtensions.cs
@@ -39,21 +39,22 @@ namespace HotChocolate.PreProcessingExtensions
         /// <param name="take"></param>
         /// <param name="includeTotalCount"></param>
         /// <returns></returns>
-        public static IOffsetPageResults<T> SliceAsOffsetPage<T>(this IEnumerable<T> items, int? skip, int take, bool includeTotalCount = true)
+        public static IOffsetPageResults<T> SliceAsOffsetPage<T>(this IEnumerable<T> items, int? skip, int? take, bool includeTotalCount = true)
             where T : class
         {
             //Do nothing if there are no results...
             if (!items.Any())
-                return new OffsetPageResults<T>(items, false, false, 0);
+                return new OffsetPageResults<T>(new List<T>(), false, false, 0);
 
             //NOTE: Implemented similar algorithm that would be used in a SQL Query; also similar to what the default
             //      HotChocolate QueryableCursorPagingHandler does...
             //NOTE: to ensure our pagination is complete, we materialize the Results!
             var skipPast = skip ?? 0;
-            var pagedResults = items.Skip(skipPast).Take(take + 1).ToList();
+            var takeSome = take ?? 0;
+            var pagedResults = items.Skip(skipPast).Take(takeSome + 1).ToList();
 
             var hasPreviousPage = pagedResults.Count > 0 && (skipPast > 0);
-            var hasNextPage = pagedResults.Count > take;
+            var hasNextPage = pagedResults.Count > takeSome;
 
             if (hasNextPage) pagedResults.Remove(pagedResults.Last());
 
