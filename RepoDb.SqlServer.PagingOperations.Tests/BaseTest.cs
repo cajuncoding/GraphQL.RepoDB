3384dc6cb37d8d8c2b393e8c31534729163e019a
Brandon Bernard <brandon.bernard@exlrt.com>
2024-05-15 02:17:24 -0500

    Optimize Cursor creation & parsing to improve performance and minimize allocations using Span<> and Pointers with NetStandard2.0 Compatibility Shims in place (still optimized)! Added Unit Test project to exercise the new cursor creation/parsing (full circle testing) code. Additional code organization improvements.

---
+++ RepoDb.SqlServer.PagingOperations.Tests/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepoDb.SqlServer.PagingOperations.Tests
+{
+    public abstract class BaseTest
+    {
+        protected BaseTest()
+        {
+            CurrentDirectory = Directory.GetCurrentDirectory();
+        }
+
+        public string CurrentDirectory { get; }
+
+        public TestContext TestContext { get; set; } = null;
+
+        public string LoadTestData(string fileName)
+        {
+            var filePath = Path.Combine(CurrentDirectory, $@"TestData\{fileName}");
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"The Test Data file [{fileName}] could not be found at: [{filePath}].", fileName);
+
+            return File.ReadAllText(Path.Combine(CurrentDirectory, filePath));
+        }
+    }
+}
---
+++ RepoDb.SqlServer.PagingOperations.Tests/RepoDbCursorHelperTests.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using RepoDb.CursorPaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace RepoDb.SqlServer.PagingOperations.Tests
+{
+    [TestClass]
+    public class RepoDbCursorHelperTests : BaseTest
+    {
+        [TestMethod]
+        public void TestCursorCreationAndParsing()
+        {
+            var testIndexes = new[] { 4, 16, 744, 7422999, int.MaxValue };
+            var createdCursors = new List<string>();
+
+            TestContext.WriteLine("Creating Cursors...");
+            foreach (var index in testIndexes)
+            {
+                var cursor = RepoDbCursorHelper.CreateCursor(index);
+                TestContext.WriteLine($"[{index}] ==> [{cursor}]");
+
+                //Assert our results are valid...
+                cursor.Should().NotBeNullOrWhiteSpace();
+                
+                createdCursors.Add(cursor);
+            }
+
+            TestContext.WriteLine("Parsing Cursors...");
+            var i = 0;
+            foreach (var cursor in createdCursors)
+            {
+                var cursorIndex = RepoDbCursorHelper.ParseCursor(cursor);
+                TestContext.WriteLine($"[{cursor}] ==> [{cursorIndex}]");
+
+                //Assert our results are valid...
+                cursorIndex.Should().BePositive();
+                cursorIndex.Should().Be(testIndexes[i++]);
+            }
+        }
+    }
+}
