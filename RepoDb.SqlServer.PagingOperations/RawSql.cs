da2d123fa96994b2978d4114f20c43ce8602e3f4
Brandon Bernard <brandon.bernard@exlrt.com>
2024-05-22 00:43:52 -0500

    - Fix Raw SQL validation logic to handle SQL that has been formatted with line breaks.

--- RepoDb.SqlServer.PagingOperations/RawSql.cs
+++ RepoDb.SqlServer.PagingOperations/RawSql.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RepoDb.SqlServer.PagingOperations
 {
     public class RawSql
     {
-        const string SELECT_PREFIX = "SELECT ";
-        const string ORDER_BY_CLAUSE = "ORDER BY";
+        private static readonly Regex SelectPrefixValidationRegex = new Regex(@"^\s*SELECT\s+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex OrderByValidationRegex = new Regex(@"\s+ORDER BY\s+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        
         public RawSql(string rawSql, object sqlParams)
         {
             var sanitizedRawSql = rawSql.Trim();
@@ -13,10 +15,10 @@ namespace RepoDb.SqlServer.PagingOperations
             if (string.IsNullOrWhiteSpace(sanitizedRawSql))
                 throw new ArgumentException("The raw sql select statement cannot be null or whitespace.");
 
-            if (!sanitizedRawSql.StartsWith(SELECT_PREFIX, StringComparison.OrdinalIgnoreCase))
+            if (!SelectPrefixValidationRegex.IsMatch(sanitizedRawSql))
                 throw new ArgumentException("The raw sql select statement provided does not appear to be a valid simple SELECT statement.");
 
-            if (sanitizedRawSql.IndexOf(ORDER_BY_CLAUSE, StringComparison.OrdinalIgnoreCase) >= 0)
+            if (OrderByValidationRegex.IsMatch(sanitizedRawSql))
                 throw new ArgumentException("The raw sql select statement cannot contains an Order By clause; Order By must be specified using the API for proper Pagination.");
 
             RawSqlStatement = sanitizedRawSql;
